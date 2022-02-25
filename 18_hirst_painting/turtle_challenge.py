c23ac1613b496494bc533a19c1a0279fb0869d62
eduardo-hyodo <sheiti.hyodo@gmail.com>
2022-02-24 22:05:18 -0300

    add: turtle random colored lines

--- 18_hirst_painting/turtle_challenge.py
+++ 18_hirst_painting/turtle_challenge.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random as r
 
 timmy_the_turtle = Turtle()
 #timmy_the_turtle.shape("turtle")
@@ -12,15 +13,29 @@ timmy_the_turtle = Turtle()
 #timmy_the_turtle.forward(100)
 #timmy_the_turtle.right(90)
 
-for i in range(0,50):
-    timmy_the_turtle.forward(5)
-    if i % 2 == 0:
-        timmy_the_turtle.penup()
-    else:
-        timmy_the_turtle.pendown()
+#for i in range(0,50):
+#    timmy_the_turtle.forward(5)
+#    if i % 2 == 0:
+#        timmy_the_turtle.penup()
+#    else:
+#        timmy_the_turtle.pendown()
 
+# figures
+#for i in range(3 , 9):
+#    grd = 360.0 / i
+#    for x in range(0,i):
+#        timmy_the_turtle.forward(25)
+#        timmy_the_turtle.right(grd)
 
-
+colors = ["black", "red", "green", "blue", "cyan", "yellow", "magenta"]
+direction = [0, 90, 180, 270]
+for i in range (1,1000):
+    speed = i /10.0
+    timmy_the_turtle.speed(i)
+    timmy_the_turtle.color(r.choice(colors))
+    timmy_the_turtle.pensize(speed)
+    timmy_the_turtle.right(r.choice(direction))
+    timmy_the_turtle.forward(30)
 
 screen = Screen()
 screen.exitonclick()
