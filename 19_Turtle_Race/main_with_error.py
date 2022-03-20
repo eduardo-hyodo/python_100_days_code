7467eb7197f38aa7b25238d198fe31fcc36a6a99
eduardo-hyodo <sheiti.hyodo@gmail.com>
2022-03-19 23:43:27 -0300

    add: turtle race

---
+++ 19_Turtle_Race/main_with_error.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+import random
+
+colors = ["red","green","blue", "black", "purple"]
+turtles = []
+screen = Screen()
+user_bet = screen.textinput(title="Make your bet", 
+                            prompt="Which turtle will win the race? enter a color: ")
+
+for i in range(0,5):
+    turtle = Turtle() 
+    turtle.penup()
+    turtle.color(colors[i])
+    turtle.setx(-1000)
+    turtle.sety(i * 50)
+    turtle.pendown()
+    turtles.append(turtle)
+
+def move_forward(turtle):
+    turtle.forward(10)
+
+def is_there_a_winner():
+    for turtle in turtles:
+        if turtle.xcor() == 600:
+            winner_turtle = turtle.pencolor()
+            print(f"The turtle {winner_turtle} is the winner")
+            if user_bet == winner_turtle:
+                print("You've won")
+            else:
+                print("You've lost!")
+            return True
+        # the else make the red to be the winner every time, even though it is not
+        else:
+            return False
+
+while not is_there_a_winner():
+    turtle_to_move = turtles[random.randint(0,4)]  
+    move_forward(turtle_to_move)
+
+screen.exitonclick()
