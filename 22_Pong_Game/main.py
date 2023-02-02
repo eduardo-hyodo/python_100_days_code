225e46c3622b37d096219c1f5217180183063a9c
Hyodo <sheiti.hyodo@gmail.com>
2023-02-02 09:33:51 -0300

    chg: paddle now inherit from Turtle

--- 22_Pong_Game/main.py
+++ 22_Pong_Game/main.py
@@ -10,33 +10,17 @@ screen.tracer(0)
 
 game_is_on = True
 
-starting_position_paddle1 = [(-200, 20), (-200, 40), (-200, 60)]
-paddle = Paddle(starting_position_paddle1)
-starting_position_paddle2 = [(200, 20), (200, 40), (200, 60)]
-paddle2 = Paddle(starting_position_paddle2)
+paddle = Paddle((-250,0))
+paddle2 = Paddle((250,0))
 
-screen.onkey(paddle.up, "w")
-screen.onkey(paddle.down, "s")
-screen.onkey(paddle2.up, "Up")
-screen.onkey(paddle2.down, "Down")
+screen.onkey(paddle.go_up, "w")
+screen.onkey(paddle.go_down, "s")
+screen.onkey(paddle2.go_up, "Up")
+screen.onkey(paddle2.go_down, "Down")
 screen.listen()
-# screen.onkey(paddle.up, "Up")
-# screen.onkey(paddle.down, "Down")
 
 while game_is_on:
     screen.update()
     time.sleep(0.195)
-    paddle.move() 
-    paddle2.move()
-
-    #Detect collision with wall
-    if paddle.head.ycor() > 290:
-        paddle.down()
-    if paddle.head.ycor() < -290:
-        paddle.up()
-    if paddle2.head.ycor() > 290:
-        paddle2.down()
-    if paddle2.head.ycor() < -290:
-        paddle2.up()
 
 screen.exitonclick()
--- 22_Pong_Game/paddle.py
+++ 22_Pong_Game/paddle.py
@@ -1,37 +1,17 @@
 from turtle import Turtle
-MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, staring_positions):
-        self.segments = []
-        self.create_paddle(staring_positions)
-        self.head = self.segments[0]
-        self.head.setheading(UP)
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(staring_positions)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
     
-    def create_paddle(self,staring_positions):
-       for position in staring_positions:
-           self.add_segment(position)
-
-    def add_segment(self, position):
-        turtle = Turtle("square")
-        turtle.color("white")
-        turtle.penup()
-        turtle.goto(position)
-        self.segments.append(turtle)
-        
-    def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
-        self.head.forward(MOVE_DISTANCE)
-
-    def up(self):
-        self.head.setheading(UP)
-
-    def down(self):
-        self.head.setheading(DOWN)
-
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
 
