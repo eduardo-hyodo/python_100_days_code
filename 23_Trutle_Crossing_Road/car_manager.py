d0927cf8f501c7925789d3500937a896a60bcaf3
Hyodo <sheiti.hyodo@gmail.com>
2023-02-06 09:24:46 -0300

    add: cars on beginning

--- 23_Trutle_Crossing_Road/car_manager.py
+++ 23_Trutle_Crossing_Road/car_manager.py
@@ -1,7 +1,29 @@
+from turtle import Turtle
+import random as rd
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+CARS_PER_LEVEL = 20
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.cars = []
+        self.speed = STARTING_MOVE_DISTANCE
+        
+    def setup(self):
+        for i in range(1,CARS_PER_LEVEL):
+            self.add_car(rd.randint(-280,280),rd.randint(-260,260))
+
+    def add_car(self,x,y):
+        car = Turtle()
+        car.shape("square")
+        car.color(rd.choice(COLORS))
+        car.penup()
+        car.goto(x,y)
+        car.setheading(180)
+        self.cars.append(car)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.forward(self.speed)
--- 23_Trutle_Crossing_Road/main.py
+++ 23_Trutle_Crossing_Road/main.py
@@ -1,7 +1,7 @@
 import time
 from turtle import Screen
 from player import Player
-# from car_manager import CarManager
+from car_manager import CarManager
 # from scoreboard import Scoreboard
 
 screen = Screen()
@@ -9,6 +9,8 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+car_manager.setup()
 
 screen.onkey(player.go_up, "w")
 screen.listen()
@@ -18,5 +20,7 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
+    car_manager.move_cars()
 
 screen.exitonclick()
