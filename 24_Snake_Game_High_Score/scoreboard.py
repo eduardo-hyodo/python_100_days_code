774b7057e24049d9bbd04ef298fd1d5ceb8bb272
Hyodo <sheiti.hyodo@gmail.com>
2023-02-20 10:01:40 -0300

    add:24_Snake_Game_High_Score

---
+++ 24_Snake_Game_High_Score/scoreboard.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT=("Arial", 12, "normal")
+
+class Scoreboard (Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0,280)
+        self.show_score()
+        self.ht()
+
+    def add_to_score(self):
+       self.score += 1
+       self.show_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0 
+        self.show_score()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     score_string = "Game Over"
+    #     self.write(
+    #             arg=score_string,
+    #             move=False,
+    #             align=ALIGNMENT,
+    #             font=FONT
+    #     )
+
+    def show_score(self):
+        self.clear()
+        score_string = f"The score: {self.score}  |  The High Score: {self.high_score}"
+        self.write(
+                arg=score_string,
+                move=False,
+                align=ALIGNMENT,
+                font=FONT
+        )
