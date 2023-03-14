848ffdb15e17baa3cba7b4652b3d31f2ea806b7a
Hyodo <sheiti.hyodo@gmail.com>
2023-03-13 23:13:41 -0400

    add: window and tomato image

---
+++ 28_Pomodoro_Tkinter/main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+canvas = Canvas(width=300, height=400)
+tomate_img = PhotoImage(file="./tomato.png")
+canvas.create_image(150,200, image=tomate_img)
+canvas.pack()
+
+
+window.mainloop()
