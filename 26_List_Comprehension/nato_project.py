f6af452c6afab259aaeb999331f9f95041d5bc1a
Hyodo <sheiti.hyodo@gmail.com>
2023-04-15 12:42:04 -0300

    add: error handler to the nato project

--- 26_List_Comprehension/nato_project.py
+++ 26_List_Comprehension/nato_project.py
@@ -2,7 +2,15 @@ import pandas as pd
 
 df_nato =  pd.read_csv("./nato_phonetic_alphabet.csv")
 ditc_nato = { row.letter:row.code for (index,row) in df_nato.iterrows()}
-word = input("Give me a word: ").upper()
-word_in_letters = list(word)
-result = [ ditc_nato[letter] for letter in word]
-print(result)
+def generate_phonetic():
+    word = input("Give me a word: ").upper()
+    word_in_letters = list(word)
+    try:
+        result = [ ditc_nato[letter] for letter in word]
+    except KeyError as key_error_message:
+        print("User only letters")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
