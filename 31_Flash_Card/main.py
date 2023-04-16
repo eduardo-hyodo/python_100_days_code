d7009fb931950ec5548bdbfd9e132a4e49f75158
Hyodo <sheiti.hyodo@gmail.com>
2023-04-16 14:24:39 -0300

    add: flip card method

--- 31_Flash_Card/main.py
+++ 31_Flash_Card/main.py
@@ -4,9 +4,10 @@ import random as rd
 
 BACKGROUND_COLOR = "#B1DDC6"
 WHITE = "#ffffff"
+BLACK = "#000000"
 FONT_LANG = ("Ariel", 40, "italic")
 FONT_WORD = ("Ariel", 60, "bold")
-pair_words = {}
+current_card = {}
 
 def unknown_word():
     next_card()
@@ -15,12 +16,22 @@ def known_word():
     next_card()
 
 def next_card():
-    global pair_words
-    pair_words = rd.choice(ditc_french_words)
+    global current_card, flip_timer
 
-    canvas.itemconfig(txt_lang, text="French") 
-    canvas.itemconfig(txt_word, text=pair_words["French"])
+    window.after_cancel(flip_timer)
+    current_card = rd.choice(ditc_french_words)
+    canvas.itemconfig(txt_lang, text="French", fill=BLACK) 
+    canvas.itemconfig(txt_word, text=current_card["French"], fill=BLACK)
+    canvas.itemconfig(canvas_image, image=img_front_card)
 
+    flip_timer = window.after(3000, flip_card)
+
+def flip_card():
+    global current_card
+
+    canvas.itemconfig(txt_lang, text="English", fill=WHITE)
+    canvas.itemconfig(txt_word, text=current_card["English"], fill=WHITE)
+    canvas.itemconfig(canvas_image, image=img_back_card)
 
 #Cards
 df_french_words =  pd.read_csv("data/french_words.csv")
@@ -33,8 +44,9 @@ window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-card_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(410,265, image=card_image)
+img_front_card = PhotoImage(file="images/card_front.png")
+img_back_card =  PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(410,265, image=img_front_card)
 txt_lang = canvas.create_text(400,150, text="", font=FONT_LANG)
 txt_word = canvas.create_text(400,263, text="", font=FONT_WORD)
 canvas.grid(column=0, row=0, columnspan=2)
@@ -46,6 +58,8 @@ btn_right.grid(column=1,row=1)
 wrong_image = PhotoImage(file="images/wrong.png")
 btn_wrong = Button(image=wrong_image, command=unknown_word, highlightthickness=0)
 btn_wrong.grid(column=0,row=1)
+
+flip_timer =  window.after(3000,flip_card)
 next_card()
 
 window.mainloop()
