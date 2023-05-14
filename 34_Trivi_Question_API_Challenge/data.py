67a65c69d88da1ea2eb8de09119321782c7223d2
Hyodo <sheiti.hyodo@gmail.com>
2023-05-14 20:28:30 -0300

    chg: url request to use params

--- 34_Trivi_Question_API_Challenge/data.py
+++ 34_Trivi_Question_API_Challenge/data.py
@@ -1,6 +1,12 @@
 import requests
 
-response =  requests.get(url="https://opentdb.com/api.php?amount=10&category=18&type=boolean")
+parameters = {
+    "amount": 10,
+    "category": 18,
+    "type": "boolean"
+}
+
+response =  requests.get(url="https://opentdb.com/api.php", params=parameters)
 
 question_data = []
 for item in response.json()["results"]:
