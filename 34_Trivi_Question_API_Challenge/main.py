efe29e8ee30ff74c9d65d7ac461ddd455e5e7971
Hyodo <sheiti.hyodo@gmail.com>
2023-05-17 20:11:27 -0300

    fix: code style

--- 34_Trivi_Question_API_Challenge/main.py
+++ 34_Trivi_Question_API_Challenge/main.py
@@ -5,9 +5,9 @@ from quiz_brain import QuizBrain
 list_question = []
 
 for question in question_data:
-   o_question = Question(question["text"], question["answer"]) 
-   list_question.append(o_question)
-   
+    o_question = Question(question["text"], question["answer"])
+    list_question.append(o_question)
+
 quiz = QuizBrain(list_question)
 while quiz.still_has_question():
     quiz.next_question()
