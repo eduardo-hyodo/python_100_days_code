9ba62c0a0a015994d5c50ece9eb4fdbecad5d596
Hyodo <sheiti.hyodo@gmail.com>
2023-08-20 11:10:15 -0300

    fix: code style

--- 34_Trivi_Question_API_Challenge/ui.py
+++ 34_Trivi_Question_API_Challenge/ui.py
@@ -1,9 +1,10 @@
-THEME_COLOR = "#375262"
-from tkinter import *
+from tkinter import Tk, Label, Button, Canvas
 from quiz_brain import QuizBrain
 
-class QuizInterface:
+THEME_COLOR = "#375262"
 
+
+class QuizInterface:
     def __init__(self, quiz: QuizBrain):
         self.quiz = quiz
         self.window = Tk()
@@ -16,11 +17,15 @@ class QuizInterface:
         self.button_true = Button(highlightthickness=0, text="True")
         self.button_false = Button(highlightthickness=0, text="False")
 
-        self.score_lbl = Label(text="Score: 0", fg="white", bg=THEME_COLOR, font=("Arial"))
+        self.score_lbl = Label(
+            text="Score: 0", fg="white", bg=THEME_COLOR, font=("Arial")
+        )
         self.score_lbl.grid(row=0, column=1)
 
-        self.question_text = self.canvas.create_text(150,50,width=200,fill=THEME_COLOR,text="Question in here")
-        self.canvas.grid(row=1, column=0,columnspan=2, pady=50)
+        self.question_text = self.canvas.create_text(
+            150, 50, width=200, fill=THEME_COLOR, text="Question in here"
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.button_true.grid(row=2, column=0)
         self.button_false.grid(row=2, column=1)
