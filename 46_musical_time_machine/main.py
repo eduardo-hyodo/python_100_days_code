f7b312dd1f022c68187725b67bd86b25a16d3d2c
Hyodo <sheiti.hyodo@gmail.com>
2024-05-01 17:59:56 -0300

    add: call url and parse list of songs

---
+++ 46_musical_time_machine/main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://www.billboard.com/charts/hot-100/2000-06-16/")
+
+# print(response.text)
+soup = BeautifulSoup(response.text, "html.parser")
+songs_tags = soup.find_all("h3", id="title-of-a-story", class_="a-no-trucate")
+
+for song in songs_tags:
+    print(song.getText())
