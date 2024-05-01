a71832104ba9ac38df7c154e04011013cacbacf7
Hyodo <sheiti.hyodo@gmail.com>
2024-05-01 17:32:19 -0300

    add: find, reverse and write to file

--- 45_web_scraping/hundred_movies_scrap.py
+++ 45_web_scraping/hundred_movies_scrap.py
@@ -3,5 +3,13 @@ import requests
 
 response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
 
-print(response.text)
+# print(response.text)
 soup = BeautifulSoup(response.text, "html.parser")
+
+movies_tags = soup.find_all("h3")
+# print(movies_tags)
+movies_tags.reverse()
+with open("movies.txt", "w") as movies:
+    for movie in movies_tags:
+        print(movie.getText())
+        movies.write(movie.getText() + "\n")
