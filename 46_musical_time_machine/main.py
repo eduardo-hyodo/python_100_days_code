51b26a18aea97c6cb6f79ad1bac0dafebe8121e9
Hyodo <sheiti.hyodo@gmail.com>
2024-05-04 02:06:08 -0300

    fix: using OAuth intead of credencial

--- 46_musical_time_machine/main.py
+++ 46_musical_time_machine/main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
 
 # PYTHONPATH=$PWD/../
 from gen_data import Data
@@ -9,9 +12,24 @@ response = requests.get("https://www.billboard.com/charts/hot-100/2000-06-16/")
 # print(response.text)
 soup = BeautifulSoup(response.text, "html.parser")
 songs_tags = soup.find_all("h3", id="title-of-a-story", class_="a-no-trucate")
+artists_tags = soup.find_all("span", class_="a-no-trucate")
 
 song_names = [song.getText().strip() for song in songs_tags]
+artists_names = [artist.getText().strip() for artist in artists_tags]
 
 access_data = Data().get()["app"]
-spotify_api_token = access_data[2]["api_key"]
-print(spotify_api_token)
+spotify_client_id = access_data[2]["api_key"]
+spotify_client_secret = access_data[2]["client_secret"]
+
+auth_manager = spotipy.SpotifyOAuth(
+    scope="playlist-modify-private",
+    client_id=spotify_client_id,
+    client_secret=spotify_client_secret,
+    redirect_uri="http://example.com",
+    show_dialog=True,
+    cache_path="token.txt",
+)
+
+sp = spotipy.Spotify(auth_manager=auth_manager)
+user_id = sp.current_user()["id"]
+# print(user_id)
