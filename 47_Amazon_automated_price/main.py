98728276d8309b135546313646b27d73c2c6d0da
Hyodo <sheiti.hyodo@gmail.com>
2024-05-12 20:51:08 -0300

    add: send email if the price if lower than a limit

--- 47_Amazon_automated_price/main.py
+++ 47_Amazon_automated_price/main.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
+import smtplib
+
+
+url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
 
 response = requests.get(
-    "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6",
+    url,
     headers={
         "User-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
         "Accept-Language": "en-US,en;q=0.5",
@@ -18,3 +21,24 @@ price = soup.find(class_="a-offscreen").get_text()
 price_without_currency = price.split("$")[1]
 price_as_float = float(price_without_currency)
 print(price_as_float)
+
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 70
+YOUR_SMTP_ADDRESS = "smtp.gmail.com"
+YOUR_EMAIL = "teste@gmail.com"
+YOUR_PASSWORD = "NeverOnCode"
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8"),
+        )
