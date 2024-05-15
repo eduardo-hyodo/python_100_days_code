97494a2b68a0f732ec6dcce5b1df6ac43da3c82f
Hyodo <sheiti.hyodo@gmail.com>
2024-05-15 01:20:39 -0300

    add: create thread and fix algorithm to use less of the webdrive to get faster.

--- 48_Selenium_Game_Playing_Bot/cookie_bot.py
+++ 48_Selenium_Game_Playing_Bot/cookie_bot.py
@@ -1,4 +1,3 @@
-from copy import Error
 import threading
 import time
 from selenium import webdriver
@@ -8,40 +7,64 @@ from selenium.common.exceptions import NoSuchElementException
 driver = webdriver.Firefox()
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 money = 0
+addons = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+addons_ids = [addon.get_attribute("id") for addon in addons]
+print(f"addons_ids = {addons_ids}")
 
 
 def click_cookie():
+    # print(f"cookie click comecou as {time.time()}")
     cookie = driver.find_element(By.ID, value="cookie")
-    for n in range(0, 300):
+    while not exit_flag.is_set():
         cookie.click()
 
 
 def get_addon():
-    while True:
+    while not exit_flag.is_set():
+        # print(f"addon comecou as {time.time()}")
         money = int(driver.find_element(By.ID, value="money").text)
         print(f"money = {money}")
-        addons = driver.find_elements(By.CSS_SELECTOR, value="#store div")
-        addon_to_get = ""
-        for addon in addons:
-            if addon.is_displayed():
-                addon_elements = addon.find_element(By.CSS_SELECTOR, value="b")
-                price = int(addon_elements.text.split("-")[1].strip().replace(",", ""))
-                if money > price:
-                    addon_to_get = addon
-        # print(addon_to_get)
-        try:
-            addon_to_get.click()
-        except AttributeError:
-            print("slip")
-        except NoSuchElementException:
-            print("no element")
-        time.sleep(5)
+
+        addon_to_get_index = ""
+        all_prices = driver.find_elements(By.CSS_SELECTOR, value="#store b")
+        addon_prices = []
+        for price_element in all_prices:
+            price = price_element.text
+            if price != "":
+                addon_prices.append(
+                    int(price_element.text.split("-")[1].strip().replace(",", ""))
+                )
+
+        for index, price in enumerate(addon_prices):
+            if money > price:
+                addon_to_get_index = index
+
+        if addon_to_get_index != "":
+            addon = driver.find_element(By.ID, value=addons_ids[addon_to_get_index])
+            if addon != "":
+                addon.click()
+                # print(f"click comecou as {time.time()}")
+                # print("Click")
+    # time.sleep(1)
 
 
+exit_flag = threading.Event()
+
+
+# print(f"comecou {time.time()}")
 click_thread = threading.Thread(target=click_cookie)
 get_addon_thread = threading.Thread(target=get_addon)
-
+# print(f"thread criadas as {time.time()}")
 click_thread.start()
 get_addon_thread.start()
 
-# driver.quit()
+time.sleep(300)
+exit_flag.set()
+
+click_thread.join()
+get_addon_thread.join()
+
+cookie_per_s = driver.find_element(by=By.ID, value="cps").text
+print(f"cookie per s = {cookie_per_s}")
+
+driver.quit()
