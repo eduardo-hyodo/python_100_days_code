31aa2101036f9cb657d11e0895ff1ef6e08fd3b8
Hyodo <sheiti.hyodo@gmail.com>
2025-08-29 20:21:30 -0300

    add: find_following to main and instant_follower

--- 52_instagram_follower_bot/instant_follower.py
+++ 52_instagram_follower_bot/instant_follower.py
@@ -30,5 +30,30 @@ class InstantFollower:
             By.XPATH, value="/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[3]"
                 )
         login_button.click()
-        time.sleep(100)
-        self.driver.quit()
+        time.sleep(5)
+        # self.driver.quit()
+
+    def find_following(self):
+        search_button = self.driver.find_element(
+            By.XPATH, value="/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div[2]/span/div/a"
+               ) 
+        search_button.click()
+    
+        time.sleep(1)
+        search_input =  self.driver.find_element(
+            By.XPATH, value="/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[1]/div/div/input"
+                )
+        search_input.send_keys("sheiti.hyodo")
+
+        time.sleep(1)
+        person_of_interisting = self.driver.find_element(
+                By.XPATH, value="/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/a"
+                )
+        person_of_interisting.click()
+
+        time.sleep(3.5)
+        following_button = self.driver.find_element(
+                By.XPATH, value="/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/section/main/div/div/header/section[3]/ul/li[3]/div/a/span"
+                )
+        following_button.click()
+        time.sleep(5)
--- 52_instagram_follower_bot/main.py
+++ 52_instagram_follower_bot/main.py
@@ -2,3 +2,4 @@ from instant_follower import InstantFollower
 
 instant_follower = InstantFollower()
 instant_follower.login()
+instant_follower.find_following()
