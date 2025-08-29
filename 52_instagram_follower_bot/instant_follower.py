c2faaa0c74a340d630fb35ecd015a85d65f66502
Hyodo <sheiti.hyodo@gmail.com>
2025-08-29 20:55:24 -0300

    chg: to use secret from secured file

--- 52_instagram_follower_bot/instant_follower.py
+++ 52_instagram_follower_bot/instant_follower.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+# PYTHONPATH=$PWD/../
+from gen_data import Data
+
 
 # PYTHONPATH=$PWD/../
 # from gen_data import Data
@@ -14,6 +17,9 @@ class InstantFollower:
 
     def login(self):
         self.driver.get("https://instagram.com")
+        access_data = Data().get()["app"]
+        instagram_secret = access_data[3]["api_key"]
+
         time.sleep(4)
         signin_button = self.driver.find_element(
             By.XPATH, value="/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[1]/div/label/input"
@@ -23,7 +29,7 @@ class InstantFollower:
         password_text = self.driver.find_element(
             By.XPATH, value="/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[2]/div/label/input"
         )
-        password_text.send_keys("placeholder")
+        password_text.send_keys(instagram_secret)
 
         time.sleep(2)
         login_button = self.driver.find_element(
@@ -57,3 +63,7 @@ class InstantFollower:
                 )
         following_button.click()
         time.sleep(5)
+        return self
+    
+    def follow(self):
+        print("teste")
