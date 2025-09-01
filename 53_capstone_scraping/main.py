9763716c7d47f5f8f08d92f4bca0b3d1138274ef
Hyodo <sheiti.hyodo@gmail.com>
2025-09-01 18:29:14 -0300

    rft: refactor main to use auxiliar classes

--- 53_capstone_scraping/main.py
+++ 53_capstone_scraping/main.py
@@ -1,29 +1,10 @@
-from bs4 import BeautifulSoup
-import requests
+from property_scrapper import PropertyScrapper 
+from form_submit import FormSubmit
 
-url = "https://appbrewery.github.io/Zillow-Clone/"
-response = requests.get(
-    url,
-    headers={
-        "User-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
-        "Accept-Language": "en-US,en;q=0.5",
-    },
-)
+ps = PropertyScrapper()
+price_list = ps.get_prices()
+url_list = ps.get_urls()
+address_list = ps.get_address()
 
-soup = BeautifulSoup(response.content, "lxml")
-# print(soup.prettify())
-
-list = soup.find_all(class_="PropertyCardWrapper__StyledPriceLine")
-price_list = [ price.getText().strip() for price in list]
-for item in price_list:
-    print(item[1:6])
-
-list = soup.find_all(class_="StyledPropertyCardDataArea-anchor")
-url_list = [ url.get("href") for url in list]
-for url in url_list:
-    print(url)
-
-list = soup.find_all('address')
-address_list = [ address.getText().strip() for address in list]
-for address in address_list:
-    print(address)
+form = FormSubmit()
+form.post_properties(price_list,url_list,address_list)
