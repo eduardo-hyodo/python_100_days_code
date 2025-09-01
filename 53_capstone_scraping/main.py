652c9c70f7b9b3315830f161df8306d91e2d53d9
Hyodo <sheiti.hyodo@gmail.com>
2025-09-01 00:01:02 -0300

    add: main, with list of price

---
+++ 53_capstone_scraping/main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://appbrewery.github.io/Zillow-Clone/"
+response = requests.get(
+    url,
+    headers={
+        "User-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
+        "Accept-Language": "en-US,en;q=0.5",
+    },
+)
+
+soup = BeautifulSoup(response.content, "lxml")
+# print(soup.prettify())
+
+list = soup.find_all(class_="PropertyCardWrapper__StyledPriceLine")
+price_list = [ price.getText().strip() for price in list]
+for item in price_list:
+    print(item[1:6])
