e5f1a58a1e1dedfd5d8642ee50fbc46ffdc8ab5f
Hyodo <sheiti.hyodo@gmail.com>
2025-09-04 17:57:22 -0300

    add: route to guess number

--- 55_decorator_flask/main.py
+++ 55_decorator_flask/main.py
@@ -9,5 +9,16 @@ def hello_world():
     print(app.number)
     return "<h1>Guess a number between 0 and 9</h1><br><image src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
 
+
+@app.route("/<int:number>")
+def check_right_number(number):
+    if number == app.number:
+        return "<h1>You found me!</h1><br><img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+    elif number > app.number:
+        return "<h1>Too high, try again</h1><br><img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+    else:
+        return "<h1>Too low, try again</h1><br><img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+
+
 if __name__ == "__main__":
     app.run()
