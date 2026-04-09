4bc12f0ffc55d49cf77ca734dac3b7a06fe92c5d
HyodoDev <hyodo.developer@gmail.com>
2026-04-09 14:04:37 -0300

    chg: list of post from request, show_post by id added

--- 59_Capstone_Project_Part_2/server.py
+++ 59_Capstone_Project_Part_2/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    blog_posts = get_blog_posts()
+    return render_template("index.html", posts=blog_posts)
 
 @app.route("/about")
 def about():
@@ -15,10 +17,19 @@ def about():
 def contact():
     return render_template("contact.html")
 
-
 @app.route("/post/<int:post_id>")
-def show_post():
-    return render_template("post.html", post_id=post_id)
+def show_post(post_id):
+    blog_posts = get_blog_posts()
+    post = next((p for p in blog_posts if p["id"] == post_id), None)
+    print(post)
+    return render_template("post.html", post=post)
+
+
+def get_blog_posts():    
+    response_blog = requests.get("https://api.npoint.io/e5d27e59c841502a02c0")
+    response_blog.raise_for_status()
+    blog_posts =  response_blog.json()
+    return blog_posts
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
